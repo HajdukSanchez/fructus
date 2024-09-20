dfa89be4675c3bc8909c340f97bb678820cc4d67
HajdukSanchez <hajduksanchez.dev@gmail.com>
2024-09-20 12:01:51 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
